83ea047ecbbbdd0af184cb9ac4fe871322215315
bacng <nhb.fit.hut@gmail.com>
2023-03-15 21:47:48 +0700

    add fatures for user

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
+  get "/signup", to:"users#new"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
