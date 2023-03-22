450ea3ed4c1bfdaf30bc373e20758e5d3ee8ae95
bacng <nhb.fit.hut@gmail.com>
2023-03-22 22:38:28 +0700

    add login features

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,12 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page.
+      reset_session
+      log_in user
+      redirect_to user
     else
       # Create an error message.
-      flash[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new', status: :unprocessable_entity
     end
   end
